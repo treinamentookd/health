a5e9adaaf9c6bc1dad7211253d933057ab8de962
Douglas Ramiro <douglas@cf.coop.br>
2020-09-18 17:29:30 -0300

    update

--- src/main/java/br/com/tecnisys/health/Endpoint.java
+++ src/main/java/br/com/tecnisys/health/Endpoint.java
@@ -1,5 +1,6 @@
 package br.com.tecnisys.health;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,18 +8,19 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 @RestController("/")
 public class Endpoint {
 
-    private final String URL_LIVENESS = "https://raw.githubusercontent.com/treinamentookd/teste/master/liveness.json";
-    private final String URL_READINESS = "https://raw.githubusercontent.com/treinamentookd/teste/master/readiness.json";
+    private final String URL_LIVENESS = "http://douglasramiro.com.br/liveness.json";
+    private final String URL_READINESS = "http://douglasramiro.com.br/readiness.json";
 
 
     @GetMapping("/readiness")
     public String readiness() throws Exception{
         Logger.getLogger(Endpoint.class.getName()).info("*****************"+ LocalDateTime.now()+" -Readiness invocado... ");
-        if (getSaudavel(URL_READINESS)){
+        if (getSaudavel(URL_READINESS).getSaudavel()){
             return "ok";
         }else{
             throw new Exception("erro");
@@ -26,18 +28,20 @@ public class Endpoint {
 
     }
 
-    private boolean getSaudavel(String url) {
+    private Retorno getSaudavel(String url) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url   ,String.class);
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
         String body = responseEntity.getBody();
-        return body.contains("true");
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readValue(body,Retorno.class);
     }
 
 
     @GetMapping("/liveness")
     public String liveness() throws Exception{
         Logger.getLogger(Endpoint.class.getName()).info("*****************"+ LocalDateTime.now()+" -Liveness invocado... ");
-        if (getSaudavel(URL_LIVENESS)){
+        if (getSaudavel(URL_LIVENESS).getSaudavel()){
             return "ok";
         }else{
             throw new Exception("erro");
---
+++ src/main/java/br/com/tecnisys/health/Retorno.java
@@ -0,0 +1,14 @@
+package br.com.tecnisys.health;
+
+public class Retorno {
+
+    private Boolean saudavel;
+
+    public Boolean getSaudavel() {
+        return saudavel;
+    }
+
+    public void setSaudavel(Boolean saudavel) {
+        this.saudavel = saudavel;
+    }
+}
